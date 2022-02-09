4af7d02d945bd9f7360127de0c72bd0a68f6c065
Wellington Nogueira <wellingtoncw7@gmail.com>
2022-02-09 19:52:47 -0300

    codigo atualizado

---
+++ src/components/RepositoryList.jsx
@@ -0,0 +1,29 @@
+import { RepositoryItem } from "./RepositoryItem"
+import '../styles/repositories.scss'
+import { useState, useEffect } from "react"
+
+
+export function RepositoryList() { // componente pai
+
+    const [repositories, setRepositories] = useState([]) // como vai ser uma lista, começa o estado com um array vazio
+
+    useEffect(() =>{
+        fetch('https://api.github.com/orgs/rocketseat/repos') // faz uma chamada pra api externa
+        .then(response => response.json()) // quando a api responder, transforma a resposta para um tipo de dado json
+        .then(data => setRepositories(data)) // dai temos os dados que serão salvos dentro da variável data que será atribuida para a variável repositories
+    }, []) // colchetes varios no final pra atualização acontecer somente uma vez
+
+
+    return (
+        <section className="repository-list">
+            <h1>Lista de repositórios</h1>
+
+            <ul>
+                {repositories.map(repository => {
+                    return <RepositoryItem key={repository.name} repository={repository} /> /* componente que pode receber informações do componente pai */
+                })}
+                
+            </ul>
+        </section>
+    )
+}
---
+++ webpack.config.js
@@ -0,0 +1,50 @@
+const path = require('path') // importação de arquivos pelo node deve ser feita com o require()
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const ReactRefreshWebpackPlugin = require('@pmmmwh/react-refresh-webpack-plugin')
+const { pathToFileURL } = require('url')
+
+const isDevelopment = process.env.NODE_ENV !== 'production'
+
+module.exports = {
+    mode: isDevelopment ? 'development' : 'production',
+    devtool: isDevelopment ? 'eval-source-map' : 'source-map',
+    entry: path.resolve(__dirname, 'src', 'index.jsx'), // arquivo de entrada (principal)
+    output: { // gera o arquivo convertido pelo webpack
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js'
+    },
+    resolve: {
+        extensions: ['.js', '.jsx'], // indica as extensões de arquivos que podem ser lidas
+    },
+    devServer: {
+        static: path.resolve(__dirname, 'public'),
+        hot: true,
+    },
+    plugins: [
+        isDevelopment && new ReactRefreshWebpackPlugin(),
+        new HtmlWebpackPlugin({
+            template: path.resolve(__dirname, 'public', 'index.html')
+        })
+    ].filter(Boolean),
+    module: { // aqui ficam as configurações de como a aplicação vai se comportar quando eu tiver importando cada um dos tipos de arquivos
+        rules: [ // array de regras
+            {
+                test: /\.jsx$/, // expressão regular que verificar se o arquivo termina com .jsx
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader', // integração entre o babel e o webpack
+                    options: {
+                        plugins: [
+                            isDevelopment && require.resolve('react-refresh/babel')
+                        ].filter(Boolean)
+                    }
+                } 
+            },
+            {
+                test: /\.scss$/, // expressão regular que verificar se o arquivo termina com .scss
+                exclude: /node_modules/,
+                use: ['style-loader', 'css-loader', 'sass-loader'] // loader que será usado pra permitir a importação de arquivos css
+            }
+        ]
+    }
+}
